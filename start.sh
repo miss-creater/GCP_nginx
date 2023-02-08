df9a9e50243796bc1369d3ffe3189c69ac132e6a
miss-creater <170750804@qq.com>
2023-02-08 17:13:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@ set -ex
 
 nginx
 rm -rf /usr/share/nginx/html/index.html
-nohup gcsfuse test-nginx /data &
-touch /data/111.txt
+gcsfuse test-nginx /data && touch /data/111.txt
 cp -p /data/test.html /usr/share/nginx/html/index.html
 chmod 755 /usr/share/nginx/html/index.html
 nginx -s reload
