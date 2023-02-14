318103aa6a796bcee6c8ec8f4da2f1f179abdb82
miss-creater <170750804@qq.com>
2023-02-14 20:01:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ set -ex
 mkdir /data
 rm -rf /usr/share/nginx/html/index.html
 gcsfuse test-nginx /data && chmod 777 /data && cp -p /data/test.html /usr/share/nginx/html/index.html
+touch /tmp/111.txt
 chmod 755 /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
